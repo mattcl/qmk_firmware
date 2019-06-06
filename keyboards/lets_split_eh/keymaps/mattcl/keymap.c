c2a95794b4f873eb81c18619f1837e397ac97564
Matt Chun-Lum <mchunlum@nerdwallet.com>
2019-06-06 09:21:06 -0700

    Fixing let's split layout

--- keyboards/lets_split_eh/keymaps/mattcl/keymap.c
+++ keyboards/lets_split_eh/keymaps/mattcl/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,       KC_Y,       KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_RCTL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,       KC_H,       KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,       KC_N,       KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER, LT(KC_SPC), LT(KC_SPC), RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER, LT(UTIL, KC_SPC), LT(UTIL, KC_SPC), RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Space Layer
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |      |  F1  |  F2  |  F3  |      |   %  |   *  |   7  |   8  |   9  |   -  |
+ * |   `  |      |  F1  |  F2  |  F3  | Bksp |   %  |   *  |   7  |   8  |   9  |   -  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |  F4  |  F5  |  F6  |      |   #  |   =  |   4  |   5  |   6  |   +  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT( \
-  _______, _______, KC_F1,  KC_F2,  KC_F3,  _______, KC_PERC, KC_ASTR, KC_7, KC_8,   KC_9,    KC_MINS, \
+  _______, _______, KC_F1,  KC_F2,  KC_F3,  KC_BSPC, KC_PERC, KC_ASTR, KC_7, KC_8,   KC_9,    KC_MINS, \
   _______, _______, KC_F4,  KC_F5,  KC_F6,  _______, KC_HASH, KC_EQL,  KC_4, KC_5,   KC_6,    KC_PLUS, \
   _______, _______, KC_F7,  KC_F8,  KC_F9,  _______, KC_CIRC, KC_ENT,  KC_1, KC_2,   KC_3,    KC_SLSH, \
   _______, _______, KC_F10, KC_F11, KC_F12,  KC_ENT, _______, _______, KC_0, KC_DOT, _______, _______  \
